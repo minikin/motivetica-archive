d3f315c399220aabc37d0124927c5c036e169235
minikin <djminikin@gmail.com>
2017-01-08 16:25:35 +0100

    Add Reachability class

--- Motivetica/ViewControllers/MainViewController.swift
+++ Motivetica/ViewControllers/MainViewController.swift
@@ -9,18 +9,42 @@
 import UIKit
 
 class MainViewController: UIViewController {
+  
+  var reachability: Reachability? = Reachability.networkReachabilityForInternetConnection()
+//  var reachability = Reachability(hostName: "www.apple.com")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+      NotificationCenter.default.addObserver(self, selector: #selector(reachabilityDidChange(_:)), name: NSNotification.Name(rawValue: ReachabilityDidChangeNotificationName), object: nil)
+      
+      _ = reachability?.startNotifier()
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    deinit {
+      NotificationCenter.default.removeObserver(self)
+      reachability?.stopNotifier()
+    }
+  
+  
+    override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+      checkReachability()
     }
     
+    func checkReachability() {
+      guard let r = reachability else { return }
+      if r.isReachable  {
+        view.backgroundColor = UIColor.green
+      } else {
+        view.backgroundColor = UIColor.red
+      }
+    }
+  
+    func reachabilityDidChange(_ notification: Notification) {
+      checkReachability()
+    }
 
     /*
     // MARK: - Navigation
