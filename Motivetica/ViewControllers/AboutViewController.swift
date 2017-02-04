490bfb553039150d191861c484c10c44327fd20b
minikin <djminikin@gmail.com>
2017-02-04 19:06:57 +0100

    Add about scene

--- Motivetica/ViewControllers/AboutViewController.swift
+++ Motivetica/ViewControllers/AboutViewController.swift
@@ -2,7 +2,7 @@
 //  AboutViewController.swift
 //  Motivetica
 //
-//  Created by Sasha Minikin on 1/30/17.
+//  Created by Sasha Prokhorenko on 1/30/17.
 //  Copyright © 2017 Sasha Prokhorenko. All rights reserved.
 //
 
@@ -10,26 +10,21 @@ import UIKit
 
 class AboutViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  @IBAction func openSpiilka(_ sender: UIButton) {
+    guard let spiilkaURL = URL(string: "http://spiilka.com") else { return }
+    UIApplication.shared.open(spiilkaURL, options: [:], completionHandler: nil)
+  }
+  
+  
+  @IBAction func openMinikin(_ sender: UIButton) {
+    guard let minikinURL = URL(string: "http://minikin.me") else { return }
+    UIApplication.shared.open(minikinURL, options: [:], completionHandler: nil)
+  }
 
 }
