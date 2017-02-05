78674782e625391239103f8197113240a7f6aa9e
minikin <djminikin@gmail.com>
2017-02-05 19:45:31 +0100

    Add basic layout implementation and minor fixes

---
+++ Motivetica/Layouts/MotivationsLayout.swift
@@ -0,0 +1,17 @@
+//
+//  MotivationsLayout.swift
+//  Motivetica
+//
+//  Created by Sasha Prokhorenko on 2/5/17.
+//  Copyright © 2017 Sasha Prokhorenko. All rights reserved.
+//
+
+import UIKit
+
+
+class MotivationsLayout: UICollectionViewLayout {
+  
+  
+  
+  
+}
--- Motivetica/ViewControllers/MotivationsViewController.swift
+++ Motivetica/ViewControllers/MotivationsViewController.swift
@@ -24,14 +24,36 @@ class MotivationsViewController: UIViewController {
         super.viewDidLoad()
       
       collectionView.dataSource = motivationsDataSourse
+      collectionView.delegate = self
       
       webservice.load(allMotivations) { result in
         print(result)
         self.motivationsDataSourse.motivations = result.value!
         self.collectionView.reloadData()
       }
+      
+      let cellWidth = UIScreen.main.bounds.size.width
+      let cellHeight = UIScreen.main.bounds.size.height - 20
+      let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+      layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
+      
+      
     }
 
+  
+//  func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+//    
+//    let numberOfCell: CGFloat = 1   //you need to give a type as CGFloat
+//    let cellWidth = UIScreen.main.bounds.size.width / numberOfCell
+//    return CGSize(width: cellWidth, height: cellWidth)
+//    
+//  }
 
+
+}
+
+extension MotivationsViewController : UICollectionViewDelegate {
+  
+  
 
 }
