4e2f4b22469c7d656e0a0b2a4930ddc4d756b0c8
minikin <djminikin@gmail.com>
2017-02-27 20:53:11 +0100

    Set colors , fonts for view

---
+++ Motivetica/Helpers/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Motivetica
+//
+//  Created by Sasha Prokhorenko on 2/27/17.
+//  Copyright © 2017 Sasha Prokhorenko. All rights reserved.
+//
+
+import Foundation
+
+enum Space : Double {
+  case betweenFooter = 8.0
+}
+
--- Motivetica/ViewControllers/MotivationsViewController.swift
+++ Motivetica/ViewControllers/MotivationsViewController.swift
@@ -19,6 +19,7 @@ class MotivationsViewController: UIViewController {
   @IBOutlet weak var allignment: UIImageView!
   @IBOutlet weak var fontName: UILabel!
   @IBOutlet weak var fontWeight: UILabel!
+  @IBOutlet var mainView: UIView!
   
   var motivations = [Motivation]()
   var motivationsDataSourse = ItemsDataSource(items:[Motivation](), cellDescriptor:{ $0.cellDescriptor })
@@ -30,7 +31,7 @@ class MotivationsViewController: UIViewController {
     motivationsCollectionView.delegate = self
   
     let cellWidth = UIScreen.main.bounds.size.width
-    let cellHeight = UIScreen.main.bounds.size.height - footerView.frame.height - 8
+    let cellHeight = UIScreen.main.bounds.size.height - footerView.frame.height - CGFloat(Space.betweenFooter.rawValue)
     let layout = motivationsCollectionView.collectionViewLayout as! MotivationsLayout
     layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
   }
@@ -51,7 +52,28 @@ class MotivationsViewController: UIViewController {
 
 extension MotivationsViewController : UICollectionViewDelegate {
 
+  func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+    
+    let motivation = motivations[indexPath.row]
+    
+    footerView.backgroundColor = UIColor(hex: motivation.backgroundColor)
+    
+    fontName.text = motivation.fontType
+    fontName.font = UIFont(name: motivation.fontType, size: 16.0)
+    fontName.textColor = UIColor(hex: motivation.fontColor)
+    
+    fontWeight.text = motivation.fontWeight
+    fontWeight.font = UIFont(name: motivation.fontType, size: 12.0)
+    fontWeight.textColor = UIColor(hex: motivation.fontColor)
 
+    
+    //    cell.fontName.text = fontType
+    //    cell.fontName.font =  UIFont(name: fontType, size: 16.0)
+    //    cell.fontName.textColor = UIColor(hex: fontColor)
+    //    cell.fontWeight.text = fontWeight
+    //    cell.fontWeight.font = UIFont(name: fontType, size: 12.0)
+    //    cell.fontWeight.textColor = UIColor(hex: fontColor)
+  }
 
 
 
